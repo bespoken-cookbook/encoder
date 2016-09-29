c629ae26d88685fd0cfac7d845487e11c4dad139
Chris <chris@xappmedia.com>
2016-09-29 18:05:42 -0400

    Fixing TSLint problems

--- server/main/index.ts
+++ server/main/index.ts
@@ -1,6 +1,6 @@
 // / <reference path="./typings/node.d.ts" />
-import * as http from 'http';
-import { Encoder } from './serverEncoder';
+import * as http from "http";
+import { Encoder } from "./serverEncoder";
 
 const PARAM_SOURCE_URL: string = "sourceUrl".toLocaleLowerCase();
 const PARAM_TARGET_BUCKET: string = "targetBucket".toLocaleLowerCase();
@@ -8,65 +8,65 @@ const PARAM_TARKET_KEY: string = "targetKey".toLocaleLowerCase();
 const PARAM_ACCESS_ID: string = "accessKeyID".toLocaleLowerCase();
 const PARAM_ACCESS_SECREY_KEY: string = "accesssecretKey".toLocaleLowerCase();
 
-var server = http.createServer(function (request: http.IncomingMessage, response: http.ServerResponse) {
-    var method = request.method;
-    var url = request.url;
-    var headers = request.headers;
-    console.info('method = ' + method + ' url = ' + url);
+let server = http.createServer(function (request: http.IncomingMessage, response: http.ServerResponse) {
+    let method = request.method;
+    let url = request.url;
+    let headers = request.headers;
+    console.info("method = " + method + " url = " + url);
     console.info(headers);
 
-    request.on('error', function (err: Error) {
+    request.on("error", function (err: Error) {
         console.error(err);
         response.statusCode = 400;
-        response.statusMessage = 'Error retrieving the request.';
+        response.statusMessage = "Error retrieving the request.";
         response.end();
     });
 
-    response.on('error', function (err: Error) {
-        console.error('There was an error sending the response.');
+    response.on("error", function (err: Error) {
+        console.error("There was an error sending the response.");
     });
 
-    if (request.method == 'POST' && request.url == '/encode') {
-        console.info('going in');
+    if (request.method === "POST" && request.url === "/encode") {
+        console.info("going in");
 
-        var body: Array<string | Buffer> = [];
+        let body: Array<string | Buffer> = [];
 
-        request.on('data', function(chunk) {
+        request.on("data", function(chunk) {
             body.push(chunk);
-        }).on('end', function() {
+        }).on("end", function() {
             try {
-                checkHeaders(headers, response)
+                checkHeaders(headers, response);
             } catch (e) {
                 response.end();
                 return;
             }
-            
-            var musicSourceUrl: string = headers[PARAM_SOURCE_URL];
-            var targetBucket: string = headers[PARAM_TARGET_BUCKET];
-            var targetKey: string = headers[PARAM_TARKET_KEY];
-            var accessKeyId: string = headers[PARAM_ACCESS_ID];
-            var accessSecret: string = headers[PARAM_ACCESS_SECREY_KEY];
-            
+
+            let musicSourceUrl: string = headers[PARAM_SOURCE_URL];
+            let targetBucket: string = headers[PARAM_TARGET_BUCKET];
+            let targetKey: string = headers[PARAM_TARKET_KEY];
+            let accessKeyId: string = headers[PARAM_ACCESS_ID];
+            let accessSecret: string = headers[PARAM_ACCESS_SECREY_KEY];
+
             Encoder.encode(musicSourceUrl, targetBucket, targetKey, accessKeyId, accessSecret, function(err: Error, url: string) {
-                console.info('ending');
+                console.info("ending");
 
                 response.statusCode = (err) ? 400 : 200;
                 response.statusMessage = (err) ? err.message : "OK";
-                response.setHeader('Content-Type', 'text/plain');
+                response.setHeader("Content-Type", "text/plain");
                 response.write(url);
                 response.end();
             });
-        })
+        });
     } else {
-        console.info('Request is not supported:\n' + request.method + '');
+        console.info("Request is not supported:\n" + request.method + "");
         response.statusCode = 404;
-        response.statusMessage = 'The requested url "' + url + '" was not found.';
-        console.info('ending');
+        response.statusMessage = "The requested url \"" + url + "\" was not found.";
+        console.info("ending");
         response.end();
     }
 });
 
-console.info('Server is listening on port 8080.');
+console.info("Server is listening on port 8080.");
 server.listen(8080);
 
 function checkHeaders(headers: any, response: http.ServerResponse) {

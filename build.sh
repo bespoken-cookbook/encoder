83d61bdcba1334650d89baa93dd51b38dd487d65
Chris <chris@xappmedia.com>
2016-10-03 15:14:50 -0400

    Base docker file. Pulling from git instead of local

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Script to convert the outputs from the encoder library to the docker and build the docker image
-## Docker doesn't allow relative paths in the build file so this is to just dump them in to the docker build area.
-
-tsc -p server/
-cp server/package.json dockerbuild/
-cp server/outputs/source/main/*.js dockerbuild/
-docker build -t bespoken/encoder dockerbuild/
